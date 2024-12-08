133e65fe975140f4fbf7b50bdf5fc2f6e4bdbe27
Jhon monsalve <99751739+Assasin1116@users.noreply.github.com>
2024-12-08 17:02:57 -0500

    opcion descargar

--- js/index.js
+++ js/index.js
@@ -15,3 +15,31 @@ function generateQRCode() {
     height: 200,
   });
 }
+function generateQRCode() {
+  const url = document.getElementById("urlInput").value;
+  if (!url) {
+    alert("Por favor, introduce una URL válida.");
+    return;
+  }
+  const qrcodeContainer = document.getElementById("qrcode");
+  qrcodeContainer.innerHTML = "";
+
+  const qrcode = new QRCode(qrcodeContainer, {
+    text: url,
+    width: 200,
+    height: 200,
+  });
+}
+
+function downloadQRCode() {
+  const qrCodeCanvas = document.querySelector("#qrcode canvas");
+  if (!qrCodeCanvas) {
+    alert("Primero genera un código QR.");
+    return;
+  }
+
+  const link = document.createElement("a");
+  link.href = qrCodeCanvas.toDataURL("image/png");
+  link.download = "codigo_qr.png";
+  link.click();
+}
